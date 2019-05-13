d6dd78927b0e6c1ef71eacd0fdcd0ac06b65f155
Leon Garcia <leon@wizeline.com>
2019-05-13 13:33:43 -0500

    Add ssh on each lab machine and not overall

--- labs/linux/provision/gen-ssh-keys-pem.sh
+++ labs/linux/provision/gen-ssh-keys-pem.sh
@@ -13,6 +13,8 @@ pushd keys
 for x in $(seq 0 $COUNT); do
   echo y | ssh-keygen -t rsa -f "${RSA_FILENAME}-${x}" -q -N "" -b 2048 -C "academy@wizeline"
   openssl rsa -in "${RSA_FILENAME}-${x}" -outform pem >"${RSA_FILENAME}-${x}.pem"
-  echo "${RSA_FILENAME}-${x}:$(cat ${RSA_FILENAME}-${x}.pub)" >>../gcloud-ssh
+  echo "academy-user-${x}:$(cat ${RSA_FILENAME}-${x}.pub)" >gcloud-ssh-${RSA_FILENAME}-${x}
+  echo gcloud compute instances add-metadata "lab-machine-${x}" --metadata-from-file ssh-keys=keys/gcloud-ssh-${RSA_FILENAME}-${x} >>../add-gcloud-ssh
+  chmod 600 *.pem "${RSA_FILENAME}-${x}" *.pub
 done
 popd
